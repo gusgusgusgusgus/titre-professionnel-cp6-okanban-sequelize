6fdd81c9910cc839a76dfa969d56df4df6b96b4c
Augustin <augustin.jaeger-seminel@oclock.school>
2025-05-16 12:40:47 +0200

    feat: added update and delete methods in List Controller

--- src/controllers/listController.js
+++ src/controllers/listController.js
@@ -35,3 +35,45 @@ export async function getOneList(req, res) {
     // Je lui demande de me retourner la réponse au format JSON
     res.json(list)
 }
+
+// Fonction pour aller mettre à jour une liste (par son identifiant présent dans la requête) en BDD
+ export async function updateList (req, res) {
+    const listId = Number.parseInt(req.params.id) 
+
+    // Je récupère les informations depuis le body
+    const body = req.body; 
+
+    // Je recherche la liste concernée par son id
+    const list = await List.findByPk(listId); 
+    // Gestion d'erreur : si elle n'existe pas, on renvoie une erreur 404
+    if (!list) {
+        return res.status(404).json({error : "List not found"})
+    }
+
+    // Je passe les informations à mettre à jour
+    const updatedList = await list.update({
+        title: body.title,
+        position: body.position,
+    }) 
+
+    // Je récupère la liste mise à jour
+    res.json(updatedList)
+ }
+
+ // Fonction pour aller supprimerune liste (par son identifiant présent dans la requête) en BDD
+ export async function deleteList (req, res) {
+    const listId = Number.parseInt(req.params.id) 
+
+    // Je recherche la liste concernée par son id
+    const list = await List.findByPk(listId);
+    // Gestion d'erreur : si elle n'existe pas, on renvoie une erreur 404
+    if (!list) {
+        return res.status(404).json({error : "List not found"})
+    } 
+
+    // Je supprime la liste
+    await list.destroy()
+    
+    // On renvoie une réponse de confirmation 204, tout en terminant la requête
+    res.status(204).end
+ }
